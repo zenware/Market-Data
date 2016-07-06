411b1459497c1db83fd459013c7b6571701f5416
RecursiveLogic <qrx9823@gmail.com>
2016-07-05 23:20:54 -0400

    More components

---
+++ client/components/HomePageHero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePageHero = (props) => {
+    return (
+        <div>
+            Awesome Stuff
+        </div>
+    )
+}
+
+export default HomePageHero
--- client/containers/Home.js
+++ client/containers/Home.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import Modal from 'boron/FlyModal'
 
 import { increment } from '../actions/counterActions'
+
+import HomePageHero from '../components/HomePageHero'
 
 class Home extends Component {
     render() {
         const { count, plusOne } = this.props
         return (
             <div>
+                <HomePageHero />
                 { count }
                 <button onClick={plusOne}>
                     Click
