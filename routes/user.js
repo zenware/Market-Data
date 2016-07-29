a8a202dc7ed5b7728b3f280458dd2d35d4c686da
RecursiveLogic <qrx9823@gmail.com>
2016-07-29 17:59:14 -0400

    Added connection to PG database for username retrieval

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
-// const config = require('../config.json')
-// const pg = require('pg')
-// const knex = require('knex')(config.knex)
+const config = require('../config.json')
+const pg = require('pg')
+const knex = require('knex')(config.knex)
 
 router.get('/:username', (req, res) => {
     res.send('You have request user', req.params.username)
 })
 
-router.post('/create', (req, res) => {
+router.post('/user/create', (req, res) => {
     res.send('Creating user with details:', req.body)
 })
 
